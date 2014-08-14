37cb8e67ae1509172e33a9aeb43a15061611e7e1
allenz8512 <allenz8512@gmail.com>
2014-08-14 21:44:41 +0800

    finish the refactory of class LoggerFactory

--- src/main/java/me/allenz/zlog/LoggerFactory.java
+++ src/main/java/me/allenz/zlog/LoggerFactory.java
@@ -2,10 +2,6 @@ package me.allenz.zlog;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
 import java.util.Properties;
 
 import android.content.Context;
@@ -18,45 +14,16 @@ import android.content.Context;
  */
 public class LoggerFactory {
 
+	private static final Logger internalLogger = ConfigureRepository
+			.getInternalLogger();
+
 	private static final String LOGGER_FACTORY_CLASS_NAME = LoggerFactory.class
 			.getName();
-
 	private static final String CONFIG_FILE_NAME = "zlog";
-	private static final String ROOT_PREFIX = "root";
-	private static final String LOGGER_PREFIX = "logger.";
-	private static final String PACKAGE_NAME_PATTERN = "([\\p{L}_$][\\p{L}\\p{N}_$]*\\.)*[\\p{L}_$][\\p{L}\\p{N}_$]*";
 
-	private static final String DEFAULT_ROOT_TAG = null;
-	private static final LogLevel DEFAULT_ROOT_LOG_LEVEL = LogLevel.DEBUG;
-
-	// prints all zlog debug messages by default
-	private static InternalLogger internalLogger = new InternalLogger(
-			LogLevel.VERBOSE);
-	private static boolean initNormally = false;
-	private static LoggerConfig rootLoggerConfig = new LoggerConfig(
-			ROOT_PREFIX, DEFAULT_ROOT_TAG, DEFAULT_ROOT_LOG_LEVEL);
-	private static Map<String, LoggerConfig> loggerConfigs;
 	private static Context appContext;
+	private static ConfigureRepository repository = new ConfigureRepository();
 	private static CallerResolver callerResolver = new CallerResolver();
-	private static HashMap<String, Logger> loggers = new HashMap<String, Logger>();
-
-	/**
-	 * Get the logger for zlog itself.
-	 * 
-	 * @return InternalLogger
-	 */
-	static InternalLogger getInternalLogger() {
-		return internalLogger;
-	}
-
-	/**
-	 * Get the root logger config.
-	 * 
-	 * @return LoggerConfig
-	 */
-	static LoggerConfig getRootLoggerConfig() {
-		return rootLoggerConfig;
-	}
 
 	/**
 	 * Initialize zlog.
@@ -65,211 +32,102 @@ public class LoggerFactory {
 	 *            the applicationContext of the app
 	 */
 	public static void init(final Context context) {
+		init(context, LogLevel.WARN);// by default, prints log that level equals
+										// or higher than WARN
+	}
+
+	/**
+	 * Initialize zlog.
+	 * 
+	 * @param context
+	 *            the applicationContext of the app
+	 * @param releaseRootLoggerLevel
+	 *            log level of the root logger in release mode
+	 */
+	public static void init(final Context context,
+			final LogLevel releaseRootLoggerLevel) {
 		synchronized (LoggerFactory.class) {
-			if (initNormally) {
-				return;
-			}
 			if (context == null) {
 				internalLogger
-						.verbose("zlog initialize failed: context is null.");
+						.warn("zlog can not read configure file because the context is null,"
+								+ " zlog will run at safe mode");
 				return;
 			}
 			appContext = context.getApplicationContext();
+			// try to setup by running environment before read config
+			checkEnvironmentAndSetup(releaseRootLoggerLevel);
 			// try to load config
-			final InputStream is = locateProperties();
-			if (is != null) {
-				loggerConfigs = new HashMap<String, LoggerFactory.LoggerConfig>();
-				try {
-					loadProperties(is);
-					initNormally = true;
-				} catch (final Exception e) {
-					internalLogger.verbose(e, "load config failed.");
-				} finally {
-					try {
-						is.close();
-					} catch (final IOException e) {
-					}
-				}
-			} else {// there's no config properties, try to judge running
-					// environment by BuildConfig.DEBUG and setup
-
+			final Properties configProperties = locateAndLoadProperties();
+			if (configProperties != null) {
+				new PropertyConfigurator(configProperties)
+						.doConfigure(repository);
 			}
-			checkInitialization();
 		}
 	}
 
 	/**
 	 * Locate the config properties file, search assets folder, res/raw in
-	 * order, read the steam of the file after found.
+	 * order, read the file after found.
 	 * 
-	 * @return the stream of config file, if file not found return {@code null}
+	 * @return the properties of config file, if file not found return
+	 *         {@code null}
 	 */
-	private static InputStream locateProperties() {
-		InputStream is = null;
+	private static Properties locateAndLoadProperties() {
+		InputStream in = null;
 		final String filename = CONFIG_FILE_NAME + ".properties";
 		try {
-			is = appContext.getAssets().open(filename);
-			internalLogger.verbose("found %s in assets", filename);
+			// search assets
+			in = appContext.getAssets().open(filename);
 		} catch (final IOException e) {
-			internalLogger.verbose("can not found %s in assets", filename);
+		}
+		if (in == null) {
+			// search res/raw
 			final String packageName = appContext.getPackageName();
 			try {
 				final int id = Utils.intReflectStaticFieldValue(packageName
 						+ ".R$raw", CONFIG_FILE_NAME, -1);
 				if (id != -1) {
-					is = appContext.getResources().openRawResource(id);
-					internalLogger.verbose("found %s in res/raw", filename);
+					in = appContext.getResources().openRawResource(id);
 				}
-			} catch (final Exception e2) {
-				internalLogger.verbose("can not found %s in res/raw", filename);
+			} catch (final Exception e) {
 			}
 		}
-		return is;
-	}
-
-	private static void loadProperties(final InputStream is) throws Exception {
+		if (in == null) {
+			return null;
+		}
+		// load the input steam into properties
 		final Properties properties = new Properties();
-		properties.load(is);
-		if (!properties.propertyNames().hasMoreElements()) {
-			throw new IOException("config file has no elements");
-		}
-		loadRootLoggerConfig(properties);
-		loadLoggerConfigs(properties);
-	}
-
-	private static void loadRootLoggerConfig(final Properties properties) {
-		final String value = (String) properties.get(ROOT_PREFIX);
-		if (value == null) {
-			return;
-		}
-		final LoggerConfig loggerConfig = parseLoggerConfig(ROOT_PREFIX, value);
-		if (loggerConfig == null) {
-			internalLogger
-					.verbose(
-							"root logger setting not specified in properties file, use default: %s",
-							rootLoggerConfig.toString());
-		} else {
-			rootLoggerConfig = loggerConfig;
-			internalLogger.verbose("setup root logger: %s",
-					loggerConfig.toString());
-		}
-	}
-
-	private static void loadLoggerConfigs(final Properties properties) {
-		for (final Enumeration<?> names = properties.propertyNames(); names
-				.hasMoreElements();) {
-			final String propertyName = (String) names.nextElement();
-			if (propertyName.startsWith(LOGGER_PREFIX)) {
-				final String name = propertyName.substring(
-						LOGGER_PREFIX.length(), propertyName.length());
-				if (name.matches(PACKAGE_NAME_PATTERN)) {
-					final String propertyValue = properties
-							.getProperty(propertyName);
-					final LoggerConfig loggerConfig = parseLoggerConfig(name,
-							propertyValue);
-					if (loggerConfig != null) {
-						internalLogger.verbose("setup logger '%s':%s", name,
-								loggerConfig.toString());
-						loggerConfigs.put(name, loggerConfig);
-					}
-				} else {
-					internalLogger
-							.verbose(
-									"name '%s' is illegal, it should be package or class fullname, skip",
-									name);
-				}
-			}
-		}
-	}
-
-	static LoggerConfig parseLoggerConfig(final String name,
-			final String propertyValue) {
-		if (Utils.isEmpty(propertyValue)) {
-			internalLogger.verbose("logger %s property value is empty, skip",
-					name);
-			return null;
-		}
-		String levelStr, tagStr = null;
-		final int comma = propertyValue.indexOf(",");
-		if (comma == -1) {
-			levelStr = propertyValue;
-		} else {
-			levelStr = propertyValue.substring(0, comma);
-			tagStr = propertyValue.substring(comma + 1);
-		}
-		final LogLevel level;
-		final String tag;
-		if (Utils.isEmpty(levelStr)) {
-			return null;
-		} else {
-			level = parseLogLevel(levelStr);
-			if (level == null) {
-				internalLogger.verbose("logger %s level '%s' is illegal, skip",
-						name, levelStr);
-				return null;
-			}
-		}
-		tag = parseTag(name, tagStr);
-		return new LoggerConfig(name, tag, level);
-	}
-
-	private static LogLevel parseLogLevel(final String level) {
 		try {
-			return LogLevel.valueOf(level.toUpperCase(Locale.ENGLISH));
-		} catch (final IllegalArgumentException e) {
+			properties.load(in);
+		} catch (final IOException e) {
 			return null;
+		} finally {
+			try {
+				in.close();
+			} catch (final IOException e) {
+			}
 		}
+		return properties;
 	}
 
 	/**
+	 * Read the value of 'package.BuildConfig.DEBUG', this value is {@code true}
+	 * in developing and is {@code false} after being packaged(release mode). In
+	 * generally, we want to hide the low level log message in release mode.
 	 * 
-	 * Parse the logger tag in property value, if the tag is empty we consider
-	 * there's no tag for the logger and the logger will use it's class name as
-	 * the tag.
-	 * <p>
-	 * 
-	 * There's no obviously limit for the length of android log tag. The size of
-	 * 'tag + message' should not be greater than 4073 bytes, and the exceed
-	 * bytes won't be written to internal log buffer.
-	 * <p>
-	 * 
-	 * In {@link android.util.Log#isLoggable(String tag, int level)}, the size
-	 * of argument 'tag' should be no greater than 23 bytes or we will receive
-	 * an exception, simplely not to call this method to avoid it.
-	 * <p>
-	 * 
-	 * Reference:
-	 * 
-	 * <pre>
-	 * http://developer.android.com/reference/android/util/Log.html
-	 * http://www.slf4j.org/android/
-	 * http://stackoverflow.com/questions/4126815/android-logging-levels
-	 * https://github.com/android/platform_frameworks_base/blob/master/core/jni/android_util_Log.cpp
-	 * https://github.com/android/platform_bionic/blob/master/libc/include/sys/system_properties.h
-	 * https://android.googlesource.com/kernel/common.git/+/android-3.4/drivers/staging/android/logger.h
-	 * https://android.googlesource.com/kernel/common.git/+/android-3.4/drivers/staging/android/logger.c
-	 * </pre>
-	 * 
-	 * @param name
-	 *            the name of the logger
-	 * @param tag
-	 *            the tag of the logger
-	 * @return If the length of the tag is zero return {@code null}, otherwise
-	 *         return the tag.
+	 * @param level
+	 *            log level of the root logger in release mode
 	 */
-	private static String parseTag(final String name, final String tag) {
-		return Utils.isEmpty(tag) ? null : tag;
-	}
-
-	/**
-	 * Check if zlog found and loaded logger config successfully, if not we
-	 * print a warnning message.
-	 */
-	private static void checkInitialization() {
-		if (!initNormally) {
-			internalLogger
-					.verbose("zlog did not initialized normally, run in default mode.");
-			initNormally = true;
+	private static void checkEnvironmentAndSetup(final LogLevel level) {
+		final String packageName = appContext.getPackageName();
+		final boolean underDevelopment = Utils.booleanReflectStaticFieldValue(
+				packageName + "BuildConfig", "DEBUG", true);
+		if (!underDevelopment) {// if the app is released
+			// shutdown internal logging
+			ConfigureRepository.setInternalLogLevel(LogLevel.OFF);
+			// reset the log level of root logger
+			repository.setRootLoggerConfig(new LoggerConfig("root", null,
+					level, false));
 		}
 	}
 
@@ -278,22 +136,18 @@ public class LoggerFactory {
 	 */
 	public static void destroy() {
 		appContext = null;
-		if (loggerConfigs != null) {
-			loggerConfigs.clear();
-			loggerConfigs = null;
-		}
-		loggers.clear();
-		loggers = null;
+		callerResolver = null;
+		repository.resetToDefault();
+		repository = null;
 	}
 
 	/**
 	 * Get the logger of the calling class.
 	 * 
-	 * @return
+	 * @return Logger
 	 */
 	public static Logger getLogger() {
 		synchronized (LoggerFactory.class) {
-			checkInitialization();
 			Logger logger = null;
 			final String caller = getCallerClassName();
 			internalLogger.verbose("Caller: %s", caller);
@@ -303,17 +157,18 @@ public class LoggerFactory {
 	}
 
 	private static Logger getDeclaredLogger(final String caller) {
-		return loggers.get(caller);
+		return repository.getLogger(caller);
 	}
 
 	private static Logger getNewLogger(final String caller) {
 		Logger logger = null;
-		if (loggerConfigs.containsKey(caller)) {
-			logger = createSimpleLogger(caller, loggerConfigs.get(caller));
+		final LoggerConfig loggerConfig = repository.getLoggerConfig(caller);
+		if (loggerConfig != null) {
+			logger = createSimpleLogger(caller, loggerConfig);
 		} else {
 			logger = createInheritParentConfigLogger(caller);
 		}
-		loggers.put(caller, logger);
+		repository.addLogger(caller, logger);
 		return logger;
 	}
 
@@ -323,29 +178,31 @@ public class LoggerFactory {
 		for (int i = caller.lastIndexOf('.'); i >= 0; i = caller.lastIndexOf(
 				'.', i - 1)) {
 			final String parentPackage = caller.substring(0, i);
-			if (loggerConfigs.containsKey(parentPackage)) {
-				logger = createSimpleLogger(caller,
-						loggerConfigs.get(parentPackage));
+			final LoggerConfig loggerConfig = repository
+					.getLoggerConfig(parentPackage);
+			if (loggerConfig != null) {
+				logger = createSimpleLogger(caller, loggerConfig);
 				parentFound = true;
 				break;
 			}
 		}
 		if (!parentFound) {
-			logger = createSimpleLogger(caller, rootLoggerConfig);
+			logger = createSimpleLogger(caller,
+					repository.getRootLoggerConfig());
 		}
 		return logger;
 	}
 
 	private static Logger createSimpleLogger(final String caller,
 			final LoggerConfig loggerConfig) {
-		final LogLevel level = loggerConfig.level;
 		final int dot = caller.lastIndexOf(".");
 		final String className = dot == -1 ? caller : caller.substring(dot + 1);
-		final String tag = loggerConfig.tag == null ? className
-				: loggerConfig.tag;
-		internalLogger.verbose("logger created: [name=%s, level=%s tag=%s]",
-				caller, level, tag);
-		return new SimpleLogger(level, tag);
+		final String tag = loggerConfig.getTag() == null ? className
+				: loggerConfig.getTag();
+		final Logger logger = new SimpleLogger(caller, tag,
+				loggerConfig.getLevel(), loggerConfig.isThread());
+		internalLogger.verbose("logger created: %s", logger);
+		return logger;
 	}
 
 	private static String getCallerClassName() {
@@ -373,51 +230,6 @@ public class LoggerFactory {
 			}
 		}
 		return null;
-	}
-
-	/**
-	 * Holder for every logger property pair.
-	 * 
-	 * @author Allenz
-	 * @since 0.1.0-RELEASE
-	 */
-	private static class LoggerConfig {
-		public String name;
-		public String tag;
-		public LogLevel level;
-
-		public LoggerConfig(final String name, final String tag,
-				final LogLevel level) {
-			this.name = name;
-			this.tag = tag;
-			this.level = level;
-		}
-
-		@Override
-		public String toString() {
-			return "[name=" + name + ", tag=" + tag + ", level=" + level + "]";
-		}
-
-	}
-
-	/**
-	 * Logger for zlog itself.
-	 * 
-	 * @author Allenz
-	 * @since 0.1.0-RELEASE
-	 */
-	static class InternalLogger extends SimpleLogger {
-
-		static final String DEFAULT_INTERNAL_LOGGER_TAG = "zlog";
-
-		public InternalLogger(final LogLevel level) {
-			super(level, DEFAULT_INTERNAL_LOGGER_TAG);
-		}
-
-		public void setLogLevel(final LogLevel level) {
-			this.level = level;
-		}
-
 	}
 
 	private static final class CallerResolver extends SecurityManager {
