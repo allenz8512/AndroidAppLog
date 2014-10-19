8e69e62484ea872a1d2ce34aa24e4ed61a8ee245
allenz8512 <allenz8512@gmail.com>
2014-10-19 11:18:58 +0800

    method printf() now return LogEvent instead of void

--- src/main/java/me/allenz/zlog/SimpleLogger.java
+++ src/main/java/me/allenz/zlog/SimpleLogger.java
@@ -12,227 +12,222 @@ import android.util.Log;
  */
 class SimpleLogger extends LoggerConfig implements Logger {
 
-	private LogWriter logWriter;
+    private LogWriter logWriter;
 
-	/**
-	 * Create a new SimpleLogger instance.
-	 * 
-	 * @param name
-	 *            class fullname of logger user
-	 * @param level
-	 *            the LogLevel of the logger
-	 * @param tag
-	 *            the tag of the logger
-	 * @param thread
-	 *            if true, shows thread name as a prefix of the tag
-	 * @param logWriter
-	 *            the writer for output log to file
-	 * @since 0.3.0-RELEASE
-	 */
-	public SimpleLogger(final String name, final String tag,
-			final LogLevel level, final boolean thread,
-			final LogWriter logWriter) {
-		super(name, tag, level, thread);
-		this.logWriter = logWriter;
-	}
+    /**
+     * Create a new SimpleLogger instance.
+     * 
+     * @param name class fullname of logger user
+     * @param level the LogLevel of the logger
+     * @param tag the tag of the logger
+     * @param thread if true, shows thread name as a prefix of the tag
+     * @param logWriter the writer for output log to file
+     * @since 0.3.0-RELEASE
+     */
+    public SimpleLogger(final String name, final String tag,
+                        final LogLevel level, final boolean thread,
+                        final LogWriter logWriter){
+        super(name, tag, level, thread);
+        this.logWriter = logWriter;
+    }
 
-	/**
-	 * Internal method that prints log message.
-	 * 
-	 * @param level
-	 *            the LogLevel of the log message
-	 * @param t
-	 *            a throwable(exception) object, can be {@code null}
-	 * @param format
-	 *            a format string of the log message, can be {@code null}.
-	 * @param args
-	 *            an array of arguments, can be {@code null}
-	 * @since 0.1.0-RELEASE
-	 */
-	protected void println(final LogLevel level, final Throwable t,
-			final String format, final Object... args) {
-		if (this.level.includes(level) && (t != null || format != null)) {
-			String message = null;
-			if (format != null && format.length() > 0) {
-				message = (args != null && args.length > 0) ? String.format(
-						format, args) : format;
-			}
-			if (t != null) {
-				message = message != null ? message + "\n"
-						+ Log.getStackTraceString(t) : Log
-						.getStackTraceString(t);
-			}
-			String tag;
-			if (thread) {// shows current thread name in tag
-				final StringBuilder sb = new StringBuilder();
-				sb.append("[").append(Thread.currentThread().getName())
-						.append("]").append(this.tag);
-				tag = sb.toString();
-			} else {
-				tag = this.tag;
-			}
-			Log.println(level.intValue(), tag, message);
-			// try to write log on disk
-			if (logWriter != null && logWriter.isStarted()) {
-				logWriter.write(level, tag, message);
-			}
-		}
-	}
+    /**
+     * Internal method that prints log message.
+     * 
+     * @param level the LogLevel of the log message
+     * @param t a throwable(exception) object, can be {@code null}
+     * @param format a format string of the log message, can be {@code null}.
+     * @param args an array of arguments, can be {@code null}
+     * @return wrapper of the log
+     * @since 0.1.0-RELEASE
+     */
+    protected LogEvent println(final LogLevel level, final Throwable t,
+                               final String format, final Object... args) {
+        if (this.level.includes(level) && (t != null || format != null)) {
+            String message = null;
+            if (format != null && format.length() > 0) {
+                message = (args != null && args.length > 0)? String.format(
+                    format, args): format;
+            }
+            if (t != null) {
+                message = message != null? message + "\n"
+                                           + Log.getStackTraceString(t): Log
+                    .getStackTraceString(t);
+            }
+            String tag;
+            if (thread) {// shows current thread name in tag
+                final StringBuilder sb = new StringBuilder();
+                sb.append("[").append(Thread.currentThread().getName())
+                    .append("]").append(this.tag);
+                tag = sb.toString();
+            } else {
+                tag = this.tag;
+            }
+            Log.println(level.intValue(), tag, message);
+            final LogEvent event = new LogEvent(level, tag, message);
+            // try to write log on disk
+            if (logWriter != null && logWriter.isStarted()) {
+                logWriter.write(event);
+            }
+            return event;
+        }
+        return null;
+    }
 
-	@Override
-	public void verbose(final String format, final Object... args) {
-		println(LogLevel.VERBOSE, null, format, args);
-	}
+    @Override
+    public void verbose(final String format, final Object... args) {
+        println(LogLevel.VERBOSE, null, format, args);
+    }
 
-	@Override
-	public void verbose(final Throwable t) {
-		println(LogLevel.VERBOSE, t, null);
-	}
+    @Override
+    public void verbose(final Throwable t) {
+        println(LogLevel.VERBOSE, t, null);
+    }
 
-	@Override
-	public void verbose(final Throwable t, final String format,
-			final Object... args) {
-		println(LogLevel.VERBOSE, t, null);
-	}
+    @Override
+    public void verbose(final Throwable t, final String format,
+                        final Object... args) {
+        println(LogLevel.VERBOSE, t, null);
+    }
 
-	@Override
-	public void verbose(final String message) {
-		println(LogLevel.VERBOSE, null, message);
-	}
+    @Override
+    public void verbose(final String message) {
+        println(LogLevel.VERBOSE, null, message);
+    }
 
-	@Override
-	public void verbose(final Throwable t, final String message) {
-		println(LogLevel.VERBOSE, t, message);
-	}
+    @Override
+    public void verbose(final Throwable t, final String message) {
+        println(LogLevel.VERBOSE, t, message);
+    }
 
-	@Override
-	public void debug(final String format, final Object... args) {
-		println(LogLevel.DEBUG, null, format, args);
-	}
+    @Override
+    public void debug(final String format, final Object... args) {
+        println(LogLevel.DEBUG, null, format, args);
+    }
 
-	@Override
-	public void debug(final Throwable t) {
-		println(LogLevel.DEBUG, t, null);
-	}
+    @Override
+    public void debug(final Throwable t) {
+        println(LogLevel.DEBUG, t, null);
+    }
 
-	@Override
-	public void debug(final Throwable t, final String format,
-			final Object... args) {
-		println(LogLevel.DEBUG, t, format, args);
-	}
+    @Override
+    public void debug(final Throwable t, final String format,
+                      final Object... args) {
+        println(LogLevel.DEBUG, t, format, args);
+    }
 
-	@Override
-	public void debug(final String message) {
-		println(LogLevel.DEBUG, null, message);
-	}
+    @Override
+    public void debug(final String message) {
+        println(LogLevel.DEBUG, null, message);
+    }
 
-	@Override
-	public void debug(final Throwable t, final String message) {
-		println(LogLevel.DEBUG, t, message);
-	}
+    @Override
+    public void debug(final Throwable t, final String message) {
+        println(LogLevel.DEBUG, t, message);
+    }
 
-	@Override
-	public void info(final String format, final Object... args) {
-		println(LogLevel.INFO, null, format, args);
-	}
+    @Override
+    public void info(final String format, final Object... args) {
+        println(LogLevel.INFO, null, format, args);
+    }
 
-	@Override
-	public void info(final Throwable t) {
-		println(LogLevel.INFO, t, null);
-	}
+    @Override
+    public void info(final Throwable t) {
+        println(LogLevel.INFO, t, null);
+    }
 
-	@Override
-	public void info(final Throwable t, final String format,
-			final Object... args) {
-		println(LogLevel.INFO, t, format, args);
-	}
+    @Override
+    public void info(final Throwable t, final String format,
+                     final Object... args) {
+        println(LogLevel.INFO, t, format, args);
+    }
 
-	@Override
-	public void info(final String message) {
-		println(LogLevel.INFO, null, message);
-	}
+    @Override
+    public void info(final String message) {
+        println(LogLevel.INFO, null, message);
+    }
 
-	@Override
-	public void info(final Throwable t, final String message) {
-		println(LogLevel.INFO, t, message);
-	}
+    @Override
+    public void info(final Throwable t, final String message) {
+        println(LogLevel.INFO, t, message);
+    }
 
-	@Override
-	public void warn(final String format, final Object... args) {
-		println(LogLevel.WARN, null, format, args);
-	}
+    @Override
+    public void warn(final String format, final Object... args) {
+        println(LogLevel.WARN, null, format, args);
+    }
 
-	@Override
-	public void warn(final Throwable t) {
-		println(LogLevel.WARN, t, null);
-	}
+    @Override
+    public void warn(final Throwable t) {
+        println(LogLevel.WARN, t, null);
+    }
 
-	@Override
-	public void warn(final Throwable t, final String format,
-			final Object... args) {
-		println(LogLevel.WARN, t, format, args);
-	}
+    @Override
+    public void warn(final Throwable t, final String format,
+                     final Object... args) {
+        println(LogLevel.WARN, t, format, args);
+    }
 
-	@Override
-	public void warn(final String message) {
-		println(LogLevel.WARN, null, message);
-	}
+    @Override
+    public void warn(final String message) {
+        println(LogLevel.WARN, null, message);
+    }
 
-	@Override
-	public void warn(final Throwable t, final String message) {
-		println(LogLevel.WARN, t, message);
-	}
+    @Override
+    public void warn(final Throwable t, final String message) {
+        println(LogLevel.WARN, t, message);
+    }
 
-	@Override
-	public void error(final String format, final Object... args) {
-		println(LogLevel.ERROR, null, format, args);
-	}
+    @Override
+    public void error(final String format, final Object... args) {
+        println(LogLevel.ERROR, null, format, args);
+    }
 
-	@Override
-	public void error(final Throwable t) {
-		println(LogLevel.ERROR, t, null);
-	}
+    @Override
+    public void error(final Throwable t) {
+        println(LogLevel.ERROR, t, null);
+    }
 
-	@Override
-	public void error(final Throwable t, final String format,
-			final Object... args) {
-		println(LogLevel.ERROR, t, format, args);
-	}
+    @Override
+    public void error(final Throwable t, final String format,
+                      final Object... args) {
+        println(LogLevel.ERROR, t, format, args);
+    }
 
-	@Override
-	public void error(final String message) {
-		println(LogLevel.ERROR, null, message);
-	}
+    @Override
+    public void error(final String message) {
+        println(LogLevel.ERROR, null, message);
+    }
 
-	@Override
-	public void error(final Throwable t, final String message) {
-		println(LogLevel.ERROR, t, message);
-	}
+    @Override
+    public void error(final Throwable t, final String message) {
+        println(LogLevel.ERROR, t, message);
+    }
 
-	@Override
-	public void wtf(final String format, final Object... args) {
-		println(LogLevel.ASSERT, null, format, args);
-	}
+    @Override
+    public void wtf(final String format, final Object... args) {
+        println(LogLevel.ASSERT, null, format, args);
+    }
 
-	@Override
-	public void wtf(final Throwable t) {
-		println(LogLevel.ASSERT, t, null);
-	}
+    @Override
+    public void wtf(final Throwable t) {
+        println(LogLevel.ASSERT, t, null);
+    }
 
-	@Override
-	public void wtf(final Throwable t, final String format,
-			final Object... args) {
-		println(LogLevel.ASSERT, t, format, args);
-	}
+    @Override
+    public void wtf(final Throwable t, final String format,
+                    final Object... args) {
+        println(LogLevel.ASSERT, t, format, args);
+    }
 
-	@Override
-	public void wtf(final String message) {
-		println(LogLevel.ASSERT, null, message);
-	}
+    @Override
+    public void wtf(final String message) {
+        println(LogLevel.ASSERT, null, message);
+    }
 
-	@Override
-	public void wtf(final Throwable t, final String message) {
-		println(LogLevel.ASSERT, t, message);
-	}
+    @Override
+    public void wtf(final Throwable t, final String message) {
+        println(LogLevel.ASSERT, t, message);
+    }
 
 }
